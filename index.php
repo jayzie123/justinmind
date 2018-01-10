c49168ed1ee1a43b85398fb47c611ee9432c4f0d
jayzie123 <34434591+jayzie123@users.noreply.github.com>
2018-01-10 19:07:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+ <img src ="screen1.PNG" height = "300px" width="300px"/>
+ <img src ="screen2.PNG" height = "300px" width="300px"/>
+ <img src ="screen3.PNG" height = "300px" width="300px"/>
+</body>
+</html> 
